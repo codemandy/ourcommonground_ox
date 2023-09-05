93dbc15355aea1ccebdd6c53fa8a3b076be28f4c
Mathias Tang <tilmathiastang@gmail.com>
2023-09-05 15:40:56 +0200

    not working

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,58 @@
+import LocomotiveScroll from 'locomotive-scroll';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+// Register ScrollTrigger with GSAP
 gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("DOMContentLoaded", function () {
-  const counterElement = document.getElementById("timeline__years");
-  counterElement.style.display = "none"; // Initially set to none
-  let cont = { val: 1800 }; // Start year
+  document.addEventListener("DOMContentLoaded", function () {
+    // Wait for the DOM to fully load
+    setTimeout(() => {
+      const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+      });
   
-  let endValue = 2021; // Default end value
-
-  function updateCounter() {
-    counterElement.innerHTML = Math.floor(cont.val);
-  }
-
-  const distance = window.innerHeight * 5;
-
-  gsap.to(cont, {
-    scrollTrigger: {
-      trigger: ".timeline__content",
-      start: "bottom center",
-      onEnter: ({trigger}) => { 
-        counterElement.style.display = "block";
-        endValue = parseInt(trigger.dataset.endYear, 10);
-        gsap.to(cont, {val: endValue, roundProps: "val", onUpdate: updateCounter, ease: "linear"});
-      },
-      onLeaveBack: () => { counterElement.style.display = "none"; },
-      end: () => `+=${distance}`,
-      scrub: 1,
-      scroller: "[data-router-wrapper]"
-    },
-    val: endValue,
-    roundProps: "val",
-    onUpdate: updateCounter,
-    ease: "linear"
+      ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+        scrollTop(value) {
+          return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+          return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+        },
+      });
+  
+      const counterElement = document.getElementById("timeline__years");
+      counterElement.style.display = "none";
+      let cont = { val: 1800 };
+  
+      function updateCounter() {
+        counterElement.innerHTML = Math.floor(cont.val);
+      }
+  
+      const targetElement = document.querySelector("[data-scroll-target='#page2']");
+  
+      if (targetElement) {
+        const distance = targetElement.offsetHeight;
+  
+        gsap.to(cont, {
+          scrollTrigger: {
+            trigger: targetElement,
+            start: "top center",
+            onEnter: () => counterElement.style.display = "block",
+            onLeaveBack: () => counterElement.style.display = "none",
+            end: `+=${distance}`,
+            scrub: 1,
+            scroller: "[data-scroll-container]",
+          },
+          val: 1893,
+          roundProps: "val",
+          onUpdate: updateCounter,
+          ease: "linear",
+        });
+      } else {
+        console.error('Target element not found');
+      }
+    }, 0);
   });
-});
+  
