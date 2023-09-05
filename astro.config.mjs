db4e3f15cde0de61642459b4d4a4c281c5b36656
Mathias Tang <tilmathiastang@gmail.com>
2023-09-05 16:43:57 +0200

    working number scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import swup from '@swup/astro';
-import ScrollPlugin from '@swup/scroll-plugin';
 import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
   integrations: [
     tailwind(),
     alpinejs(),
-    swup({
-      smoothScrolling: true,
-      debug: true,
-      plugins: [
-        new ScrollPlugin({
-          doScrollingRightAway: false,
-          animateScroll: true,
-          scrollFriction: 0.3,
-          scrollAcceleration: 0.04,
-        }),
-      ],
-    }),
   ],
 });
--- scripts/main.js
+++ scripts/main.js
@@ -4,35 +4,40 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", function () {
-  const counterElement = document.getElementById("timeline__years");
-  counterElement.style.display = "none"; // Initially set to none
-  let cont = { val: 1800 }; // Start year
   
-  let endValue = 2021; // Default end value
+  function initCounter(triggerSelectorStart, triggerSelectorEnd, startVal, endVal) {
+    let cont = { val: startVal };
+    const counterElement = document.getElementById("timeline__years");
+    
+    function updateCounter() {
+      counterElement.innerHTML = Math.floor(cont.val);
+    }
 
-  function updateCounter() {
-    counterElement.innerHTML = Math.floor(cont.val);
+    gsap.to(cont, {
+      scrollTrigger: {
+        trigger: triggerSelectorStart,
+        start: "top top",
+        endTrigger: triggerSelectorEnd,
+        end: "center center",
+        onLeave: () => {
+          counterElement.style.display = "block";
+          gsap.to(cont, {val: endVal, roundProps: "val", onUpdate: updateCounter, ease: "linear", duration: 0.5});
+        },
+        onEnterBack: () => { counterElement.style.display = "none"; },
+        onEnter: () => { counterElement.style.display = "block"; },
+        scrub: 1,
+        scroller: "[data-router-wrapper]"
+      },
+      val: endVal,
+      roundProps: "val",
+      onUpdate: updateCounter,
+      ease: "linear"
+    });
   }
 
-  const distance = window.innerHeight * 5;
+  // First counter from 1800 to 1893
+  initCounter('.page2', '.page2', 1800, 1893);
 
-  gsap.to(cont, {
-    scrollTrigger: {
-      trigger: ".timeline__content",
-      start: "bottom center",
-      onEnter: ({trigger}) => { 
-        counterElement.style.display = "block";
-        endValue = parseInt(trigger.dataset.endYear, 10);
-        gsap.to(cont, {val: endValue, roundProps: "val", onUpdate: updateCounter, ease: "linear"});
-      },
-      onLeaveBack: () => { counterElement.style.display = "none"; },
-      end: () => `+=${distance}`,
-      scrub: 1,
-      scroller: "[data-router-wrapper]"
-    },
-    val: endValue,
-    roundProps: "val",
-    onUpdate: updateCounter,
-    ease: "linear"
-  });
+  // Second counter from 1893 to 1955
+  initCounter('.page2', '.page22', 1893, 1955);
 });
